b3ade7f0789d98bae25aa1e87f598f25ec130ea6
voideli <eblackburnjr@gmail.com>
2023-08-24 10:35:07 -0400

    Login page

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function savelogininfo()
+{
+  var username = document.getElementById("userInput").value;
+  var password = document.getElementById("passInput").value;
+
+  window.location.href = "../pages/home.html";
+
+}
